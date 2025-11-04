149f7b062deb2431539eabdd455ecb97bb02190f
Vinicius <kdo.vinI@gmail.com>
2025-11-03 22:01:43 -0300

    MVP prod

---
+++ .vercel/output/static/_app/immutable/entry/start.VsdBpnlB.js
@@ -0,0 +1,1 @@
+import{a as r}from"../chunks/C_vIvpGJ.js";import{y as t}from"../chunks/qixkw80f.js";export{t as load_css,r as start};
---
+++ .vercel/output/static/_app/immutable/nodes/15.D5twNU32.js
@@ -0,0 +1,1 @@
+import{s}from"../chunks/DUJQqtHE.js";import{S as t,i as e}from"../chunks/CBbGAyDJ.js";class l extends t{constructor(o){super(),e(this,o,null,null,s,{})}}export{l as component};
